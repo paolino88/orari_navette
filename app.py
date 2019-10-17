443c834069bc8568df22a65cafe4c41b3b01b621
Paolino Iorio <paolino.iorio@eni.com>
2019-10-17 11:03:13 +0200

    commit 100

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -11,5 +11,5 @@ def index():
 
 
 
-
-if __name__ == '__main__': app.run(debug=True)
+if __name__ == '__main__':
+    app.run(use_reloader = True,debug=True)
